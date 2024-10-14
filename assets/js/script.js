513ab077bd83045463dcb735f496ff3b24e4dd6f
unknown <matak12m@seznam.cz>
2024-10-14 10:28:03 +0100

    comet function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 
 
-
-
 const back_canvas = document.getElementById("background_canvas");
 const front_canvas = document.getElementById("planets_canvas");
 
@@ -17,8 +15,11 @@ front_canvas.height = 1500;
 ctx1.imageSmoothingEnabled = false;  //makes the pixelart assets crisp
 ctx2.imageSmoothingEnabled = false;  //makes the pixelart assets crisp
 
+let transX = front_canvas.width * 0.5,
+    transY = front_canvas.height * 0.5;
 
-
+ctx1.translate(transX, transY);
+ctx2.translate(transX, transY);
 
 // //Render planet Mars
 // T5.contextforeground.beginPath();
@@ -33,17 +34,22 @@ ctx2.imageSmoothingEnabled = false;  //makes the pixelart assets crisp
 //change the multiplication of the square root to change the curve of approach
 //change 60 to 24 if needed for days - only if theres a huge meteor that changes the days, or the changing of sun texture
 
+
+
+const sunRadius = 100;
+
+
 let currentTime;
 let hours;
 let minutes;
 let seconds;
 let miliseconds;
 let position;
-let planetPosition;
+let timePosition;
 
 
 
-function CometPosition(){
+function getTime(){
     currentTime = new Date();
     hours = currentTime.getHours();
     minutes = currentTime.getMinutes();
@@ -51,18 +57,16 @@ function CometPosition(){
     miliseconds = currentTime.getMilliseconds();
 
 
-    //minutes = minutes + seconds/60 + miliseconds/60000;
+    minutes = minutes + seconds/60 + miliseconds/60000;
 
-    minutes = 30
-    
+    console.log(minutes)
 
-    let position = 60 - Math.sqrt(minutes)*8;
-    console.log(minutes);
-
-    return position;
+    return minutes;
    
 
 }
+
+
 
 
 
@@ -89,26 +93,61 @@ update();
 function draw() {
 
     ctx1.fillStyle = "black";
-    ctx1.fillRect(0, 0, back_canvas.width, back_canvas.height);
+    ctx1.fillRect(-front_canvas.width *0.5, -front_canvas.height *0.5, back_canvas.width, back_canvas.height);
 
 
 
 ctx1.beginPath();
-ctx1.arc(back_canvas.width/2, back_canvas.height/2 ,100, 0, 2 * Math.PI);
+ctx1.arc(0, 0 ,sunRadius, 0, 2 * Math.PI);
 ctx1.fillStyle = "orange";
 ctx1.fill();
 
+ctx2.clearRect(-front_canvas.width *0.5, -front_canvas.height *0.5, front_canvas.width, front_canvas.height)
 
-planetPosition = CometPosition();
 
 
-    ctx2.clearRect(0, 0, front_canvas.width, front_canvas.height)
 
-ctx2.beginPath();
-ctx2.arc(front_canvas.width/2 + planetPosition *10, front_canvas.height/2 + planetPosition*10 , 50, 0, 2*Math.PI);
-ctx2.fillStyle = "blue";
-ctx2.fill();
+Comet(0, 60, 60, "blue");
+
+
+
+
+Comet(90, 20, 2, "green");
+
 
 
 }
 
+
+
+
+
+function Comet(addAngle, timeToCollide, rotationSlowness, colour) {
+
+
+
+timePosition = getTime();
+
+
+    let radius = timeToCollide - timePosition;  //getting closer to the sun as time goes on, may be moved.
+
+    let angle = (timePosition % rotationSlowness) * 6 + addAngle;  // angle in 360 degrees, affected by function value
+
+    let sinus = Math.sin(angle /( 2 * Math.PI));  //converted from radians to degrees
+    let cosinus = Math.cos(angle /( 2 * Math.PI));
+
+
+
+    //console.log(radius);
+    //console.log(angle)
+    //console.log(sinus);
+    //console.log(cosinus);
+
+    
+ctx2.beginPath();
+ctx2.arc((radius * 12.5 + (sunRadius/2)) * sinus  , (radius* 12.5 + (sunRadius/2))*cosinus  , 50, 0, 2*Math.PI);
+ctx2.fillStyle = colour;
+ctx2.fill();
+
+
+}
